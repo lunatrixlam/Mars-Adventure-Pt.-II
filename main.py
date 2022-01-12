ec7cc984f845fb70b3cb84bd8ad33d34677351e7
Luna Lam <93636920+lunatrixlam@users.noreply.github.com>
2022-01-12 23:54:42 +0000

    changed ln 29 to observation.split() so we can iterate through the input string to accurately count each observation category instance

--- main.py
+++ main.py
@@ -26,18 +26,16 @@ while True:     # until no more observations, ask user for input and then note t
 
     found_category = False
 
-    for word in geographic:
-        if word in observation:
+    for word in observation.split():    # splits the string into a list so we can iterate over
+        if word in geographic:
             geographic_count += 1
             found_category = True
-
-    for word in life:
-        if word in observation:
+        elif word in life:
             life_count += 1
             found_category = True
-    
-    if found_category == False:
-        boring_count += 1
+        else: 
+            if found_category == False:
+                boring_count += 1
 
 print()
 print("Finished observations.")
@@ -47,77 +45,77 @@ print(f"Boring observations: {boring_count}")
 print()
 print()
 
-print("You are about to land on Mars!")
-time.sleep(2)
-print("...")
-time.sleep(2)
-print("The landing was successful.")
-time.sleep(2)
-print("Time to disembark!")
-time.sleep(2)
-print("...but first, you need to enter the door code to unlock the ship.")
-print()
+# print("You are about to land on Mars!")
+# time.sleep(2)
+# print("...")
+# time.sleep(2)
+# print("The landing was successful.")
+# time.sleep(2)
+# print("Time to disembark!")
+# time.sleep(2)
+# print("...but first, you need to enter the door code to unlock the ship.")
+# print()
 
-correct_code = str(random.randint(1000, 9999))  # grabs a random integer between 1000-9999 and returns it as a str so we can use the .isdigit() method later
+# correct_code = str(random.randint(1000, 9999))  # grabs a random integer between 1000-9999 and returns it as a str so we can use the .isdigit() method later
 
-num_guesses = 0     # initializes a counter variable to track guesses
+# num_guesses = 0     # initializes a counter variable to track guesses
 
-print(f"Correct Code: {correct_code}")
-print()
+# print(f"Correct Code: {correct_code}")
+# print()
 
-while True:
+# while True:
 
-    guessed_code = input("Enter doorcode: ")
+#     guessed_code = input("Enter doorcode: ")
 
-    if guessed_code.isdigit():
-        if guessed_code == correct_code:
-            break
-        else:
-            print("Sorry, that's wrong.")
-            num_guesses += 1
-            print(f"Wrong Guesses: {num_guesses}")
-    else:
-        print("Invalid door code. Enter 4 digits only.")
+#     if guessed_code.isdigit():
+#         if guessed_code == correct_code:
+#             break
+#         else:
+#             print("Sorry, that's wrong.")
+#             num_guesses += 1
+#             print(f"Wrong Guesses: {num_guesses}")
+#     else:
+#         print("Invalid door code. Enter 4 digits only.")
 
-print("We're in. Welcome to Mars!")
-print()
-time.sleep(2)
+# print("We're in. Welcome to Mars!")
+# print()
+# time.sleep(2)
 
-# A duel with Darth COBOL 
+# # A duel with Darth COBOL 
 
-print("Oh, no! It's Darth COBOL! Time to duel.")
-print()
+# print("Oh, no! It's Darth COBOL! Time to duel.")
+# print()
 
-darth_cobol_health = 3
+# darth_cobol_health = 3
 
-while darth_cobol_health > 0:
+# while darth_cobol_health > 0:
     
-    return_fire = input("Press RETURN to fire: ")
+#     return_fire = input("Press RETURN to fire: ")
 
-    if return_fire == "":
-        if random.randint(1, 2) == 1:
-            print("You missed!")
-            print(f"Darth COBOL health: {darth_cobol_health}")
-            print()
-        else:
-            print("You hit Darth COBOL!")
-            darth_cobol_health -= 1
-            print(f"Darth COBOL health: {darth_cobol_health}")
-            print()
-print("You win!")
-print()
+#     if return_fire == "":
+#         if random.randint(1, 2) == 1:
+#             print("You missed!")
+#             print(f"Darth COBOL health: {darth_cobol_health}")
+#             print()
+#         else:
+#             print("You hit Darth COBOL!")
+#             darth_cobol_health -= 1
+#             print(f"Darth COBOL health: {darth_cobol_health}")
+#             print()
+# print("You win!")
+# print()
 
-# Take-off from Mars and return home. Taking off from Mars is harder because of magic gamma rays so your spaceship needs to do the liftoff routine 3 times in a row.
+# # Take-off from Mars and return home. Taking off from Mars is harder because of magic gamma rays so your spaceship needs to do the liftoff routine 3 times in a row.
 
-print("This was a lot of fun. Now it's time to go home!")
-print("Prepare for lift-off...")
-print()
+# print("This was a lot of fun. Now it's time to go home!")
+# print("Prepare for lift-off...")
+# print()
 
-for i in range(3): # you can also use a While loop and initialize an attempt counter = 3 and nest the timer loop in the attempt While loop
-    timer = 5
+# for i in range(3): # you can also use a While loop and initialize an attempt counter = 3 and nest the timer loop in the attempt While loop
+#     timer = 5
 
-    while timer > 0:
-        print(timer, "...")
-        time.sleep(2)
-        timer -= 1
-print("*** LIFT-OFF ***")
+#     while timer > 0:
+#         print(timer, "...")
+#         time.sleep(2)
+#         timer -= 1
+# print("*** LIFT-OFF ***")
